ff79c2e8fcc1b7e8307b42e79b75d08e014390ed
Maxim Loboda <wi4er777@gmail.com>
2022-12-30 10:03:50 +0200

    init

---
+++ public/javascripts/Authorization-form.js
@@ -0,0 +1,14 @@
+
+
+class AuthorizationForm extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+}
+
+
+globalThis.customElements.define('authorization-form', AuthorizationForm);
+
+
